9782da4b65c07bb76a0157263cf4e216e931c8dd
Emma <em.lebedeva85@gmail.com>
2023-05-24 11:42:49 +0300

    Create Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Random random = new Random(); 
+int number = random.Next(100, 1000); 
+int digit = ((number/10)%10);
+
+
+Console.WriteLine(number);
+Console.WriteLine(digit);
+
+
+
