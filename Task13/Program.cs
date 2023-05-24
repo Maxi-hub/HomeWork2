6abc0fff5166736bd663c19f8c526864a38942ae
Emma <em.lebedeva85@gmail.com>
2023-05-24 12:24:25 +0300

    Create Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+        
+if (number < 100)
+{
+  Console.WriteLine("третьей цифры нет");
+}
+
+else {
+    string digit = number.ToString();
+    Console.WriteLine(digit[2]);
+}
+  
